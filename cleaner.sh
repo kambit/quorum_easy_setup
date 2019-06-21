ed967759b89f8d320e5650cf85c1c609d6eaaa55
kambit <xoiiikoocoding@gmail.com>
2019-06-21 14:53:04 +1000

    update

--- cleaner.sh
+++ cleaner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker rm -f $(docker ps -a | grep “quorum” | awk ‘{print $1}’)
-docker rm -f $(docker ps -a | grep "istanbul-tools" | awk ‘{print $1}’)
+docker rm -f $(docker ps -a | grep "quorum" | awk '{print $1}')
+docker rm -f $(docker ps -a | grep "istanbul-tools" | awk '{print $1}')
 sudo docker network remove $(sudo docker network ls -q)
 sudo rm -rf qdata_*
 sudo rm -rf istanbul_dir
